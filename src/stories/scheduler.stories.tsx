8476faf5435d73c41f23e49671083b233b011297
Parichay <parichay.barpanda@gmail.com>
2025-10-10 13:41:12 +0530

    update pkgs

--- src/stories/scheduler.stories.tsx
+++ src/stories/scheduler.stories.tsx
@@ -9,7 +9,7 @@ export default {
 export function SchedulerDemo() {
   const [cronExp, setCronExp] = React.useState('0 0 * * *');
   const [cronError, setCronError] = React.useState(''); // get error message if cron is invalid
-  const [isAdmin, setIsAdmin] = React.useState(false); // set admin or non-admin to enable or disable high frequency scheduling (more than once a day)
+  const [isAdmin, setIsAdmin] = React.useState(true); // set admin or non-admin to enable or disable high frequency scheduling (more than once a day)
 
   return (
     <Scheduler cron={cronExp} setCron={setCronExp} setCronError={setCronError} isAdmin={isAdmin} />
